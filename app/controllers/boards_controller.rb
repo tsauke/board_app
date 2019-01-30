d7a9dfc049ba786825951014b4ea1e30a2a8f1cc
tsauke <tasukesuke@gmail.com>
2019-01-30 16:12:34 +0900

    add resources

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,22 @@
+class BoardsController < ApplicationController
+
+  def index
+    @boards = Board.all
+  end
+  def new
+    @board = Board.new
+  end
+
+  def create
+    Board.create(board_params)
+  end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  private
+
+  def board_params
+  end
+end
